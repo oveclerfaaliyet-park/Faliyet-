e6210f5ba2f90ab7db4cc945ea9b2c6d251fabf0
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-01 23:55:45 +0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,24 +1,16 @@
-const CACHE = "park-v1";
-
-const FILES = [
-  "index.html",
-  "kayit.html",
-  "ayarlar.html",
-  "style.css",
-  "app.js",
-  "manifest.json",
-  "icon-192.png",
-  "acilis.mp4"
-];
-
 self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(FILES))
+    caches.open("park").then(c=>c.addAll([
+      "./",
+      "index.html",
+      "style.css",
+      "app.js"
+    ]))
   );
 });
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
