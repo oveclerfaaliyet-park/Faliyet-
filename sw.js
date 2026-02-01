71193b994b4aec5bd34ebd1616be4494eaa5d0d9
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-02 00:53:04 +0300

    Refactor service worker install and fetch event listeners

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-self.addEventListener("install", e=>{
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("park").then(c=>c.addAll([
-      "./",
-      "index.html",
-      "style.css",
-      "app.js"
-    ]))
+    caches.open("park-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./acilis.mp4",
+        "./manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
