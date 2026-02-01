66072eaf065907cbfad57bfd38fc5b7a452c45f7
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-02 01:21:48 +0300

    Simplify service worker fetch event handling
    Removed caching logic and simplified fetch event handling.

--- sw.js
+++ sw.js
@@ -1,20 +1,3 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("park-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./acilis.mp4",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", e=>{
+  e.respondWith(fetch(e.request));
 });
