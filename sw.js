804db06fce98416830820eb9ab458b8702a860a4
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-02 23:29:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = "park-cache-v1";
-const urlsToCache = ["./index.html","./manifest.json","./sw.js"];
-
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(resp=>resp || fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
 });
