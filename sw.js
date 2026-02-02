a2c3a65198112a3184380f5390f6335ce1c3fcff
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-02 03:33:24 +0300

    Refactor service worker caching mechanism
    Updated service worker to include additional cached images and changed cache name.

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = "faaliyet-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./acilis.mp4",
-  "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('faaliyet-cache').then(cache=>{
+      return cache.addAll([
+        '.',
+        'index.html',
+        'manifest.json',
+        'acilis.mp4',
+        'pexels-eberhardgross-1301976.jpg',
+        'pexels-eberhardgross-1612351.jpg',
+        'pexels-skylar-kang-6044198.jpg'
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
