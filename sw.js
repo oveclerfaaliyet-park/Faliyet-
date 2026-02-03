2659962df8405caaba13977690691579619d971e
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-03 10:45:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open("park").then(c=>{
+return c.addAll([
+"index.html",
+"manifest.json",
+"acilis.mp4"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(r=>{
+return r || fetch(e.request);
+})
+);
 });
