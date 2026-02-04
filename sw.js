610d58c4fbf4d191e0167daf555800695535eaa1
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-04 07:23:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('install', e=>{console.log('SW yüklendi');});
-self.addEventListener('activate', e=>{console.log('SW aktif');});
-self.addEventListener('fetch', e=>{});
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("takip-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./park.html",
+        "./personel.html",
+        "./evrak.html",
+        "./settings.html",
+        "./style.css",
+        "./app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
