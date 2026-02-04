1265d64bdd848c25346ca24fd0e9b34951f05616
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-04 09:01:29 +0300

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,14 +1,13 @@
 function pdfOlustur(){
-  let s = pdfSayfa.value;
-  fetch(API_URL+"?sayfa="+encodeURIComponent(s))
+  fetch(API_URL+"?sayfa="+pdfSayfa.value)
   .then(r=>r.json()).then(d=>{
     const { jsPDF } = window.jspdf;
     let doc=new jsPDF();
     let y=10;
     d.veriler.forEach(r=>{
       doc.text(r.join(" | "),10,y);
-      y+=7;
+      y+=8;
     });
-    doc.save(s+".pdf");
+    doc.save(pdfSayfa.value+".pdf");
   });
 }
