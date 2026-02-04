a3e9f18fe9272f011ad65b14473385611c68355e
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-04 12:21:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,39 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(
-    caches.open('saha-cache').then(cache=>{
-      return cache.addAll(['/','index.html','style.css','app.js']);
-    })
-  );
+const CACHE_NAME = "saha-takip-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/park.html",
+    "/personel.html",
+    "/evrak.html",
+    "/ayarlar.html",
+    "/style.css",
+    "/app.js"
+];
+
+// INSTALL
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache=>cache.addAll(urlsToCache))
+    );
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
-  );
+
+// ACTIVATE
+self.addEventListener("activate", e=>{
+    e.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(
+                keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+            );
+        })
+    );
+});
+
+// FETCH
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
 });
