21444f1a744ba30c71af829bebc7c42dce240c86
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-04 08:12:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,14 @@
-const CACHE_NAME = "takip-v2";
-
-self.addEventListener("install", e => {
+const CACHE="takip-v1";
+self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./park.html",
-        "./personel.html",
-        "./evrak.html",
-        "./settings.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json"
-      ]);
-    })
+    caches.open(CACHE).then(c=>c.addAll([
+      "./","./index.html","./park.html",
+      "./personel.html","./evrak.html",
+      "./settings.html","./app.js",
+      "./pdf.js","./settings.js","./style.css"
+    ]))
   );
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
