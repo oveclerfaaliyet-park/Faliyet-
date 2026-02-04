e660ffcf6f26c903ac64b38f857b41e15335c11b
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-02-04 09:43:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("saha").then(c=>c.addAll([
-      "index.html","park.html","personel.html","evrak.html",
-      "settings.html","app.js","style.css"
-    ]))
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
+self.addEventListener('install', e=>{ e.waitUntil(caches.open('app-v1').then(c=>c.addAll(['/','index.html','app.js','style.css']))); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
