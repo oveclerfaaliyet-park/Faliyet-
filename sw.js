ecac490ba2607ec4e605453951dbb0726900ce58
oveclerfaaliyet-park <oveclerfaaliyet@gmail.com>
2026-05-19 12:42:12 +0300

    Refactor service worker by removing cache handling
    Removed caching logic and event listeners from service worker.

--- sw.js
+++ sw.js
@@ -1,39 +1,9 @@
-const CACHE_NAME = "saha-takip-cache-v1";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/park.html",
-    "/personel.html",
-    "/evrak.html",
-    "/ayarlar.html",
-    "/style.css",
-    "/app.js"
-];
+self.addEventListener('install',(e)=>{
 
-// INSTALL
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache=>cache.addAll(urlsToCache))
-    );
+  self.skipWaiting();
+
 });
 
-// ACTIVATE
-self.addEventListener("activate", e=>{
-    e.waitUntil(
-        caches.keys().then(keys=>{
-            return Promise.all(
-                keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-            );
-        })
-    );
-});
+self.addEventListener('fetch',(e)=>{
 
-// FETCH
-self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(response=>{
-            return response || fetch(e.request);
-        })
-    );
 });
